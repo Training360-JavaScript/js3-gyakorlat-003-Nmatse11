ccd1c18fb29b53b59b9bd6ee4ae4672d023526fd
Nmatse <atseniko@gmail.com>
2021-11-10 23:17:19 +0100

    ready

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,8 @@
-
+'use strict';
+const setCookie = (value) => {
+  const now = new Date();
+  now.setTime(now.getTime() + (15 * 60 * 1000));
+  let expires = now.toUTCString();
+  document.cookie = `token=${value};expires=${expires}`;
+}
+export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,36 @@
-
+'use strict';
+const cookieHandler = {
+  setCookie(name, value) {
+    const now = new Date();
+    now.setTime(now.getTime() + (15 * 60 * 1000));
+    let expires = now.toUTCString();
+    //document.cookie = `${name}=${value};expires=${expires}`;
+  },
+  getAll() {
+    //const listCookies = document.cookie.split('; ');
+    let cookies = {};
+    for (let i = 0; i < listCookies.length; i++) {
+      let cookie = listCookies[i].split("=");
+      cookies[(cookie[0])] = (cookie.slice(1).join(': '));
+    }
+    return cookies;
+  },
+  toSessionStorage() {
+    //const listCookies = document.cookie.split('; ');
+    for (let i = 0; i < listCookies.length; i++) {
+      let cookie = listCookies[i].split("=");
+      sessionStorage.setItem([cookie[0]], cookie[1])
+    }
+  },
+  flush() {
+    //let cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      let name = cookies[i];
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  }
+}
+cookieHandler.setCookie('viewed', '5');
+cookieHandler.setCookie('uid', '354774631237');
+cookieHandler.setCookie('ssid', 'Bx55OWbHJ0Vt_IGIF');
+export { cookieHandler };
